ee7c1c573a929e62a219cce615f104e715ef6c0c
Francisco Carriedo Scher <fcarriedos@gmail.com>
2018-04-06 18:50:54 -0400

    Website initial commit.

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  // .get('/', (req, res) => res.render('pages/index'))
-  .get('/caca', (req, res) => sendSoapRequest(res))
-  .post("/pedo", (req, res) => catchConversationUpdate(req, res))
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
