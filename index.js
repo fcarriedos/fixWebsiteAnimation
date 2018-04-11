c48349478a0a9362c3f95481ba5359791a7504e9
Francisco Carriedo Scher <fcarriedos@gmail.com>
2018-04-11 08:05:32 -0400

    Instant online shop bot functionality starting

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var restify = require('restify');
+// var restify = require('restify');
 
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+var webapp = express();
+
+// Web pages
+webapp
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .get('/', (req, res) => res.render('pages/index'));
 
+// Bot endpoints
+webapp.post('/instant-online-shop', (req, res) => res.render('pages/index'));
+
+// And starting!
+webapp.listen(PORT, () => console.log(`Listening on ${ PORT }`);
