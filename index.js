aa10138a341442510267a360a4d84b8ea4d4c204
Francisco Carriedo Scher <fcarriedos@gmail.com>
2018-04-11 09:46:47 -0400

    Adding webhook verification endpoint

--- index.js
+++ index.js
@@ -10,6 +10,35 @@ var webapp = express();
 webapp.use(bodyParser.json());
 webapp.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Webhook verification
+webapp.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "37mej3wg9JR3bhAcE40e8fd9D4Es384P8GkEHFpW3S92yU"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
 // Web pages
 webapp
   .use(express.static(path.join(__dirname, 'public')))
