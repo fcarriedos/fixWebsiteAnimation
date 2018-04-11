25025bb4cb7fdd8cfdcdba5581041a32c9bc91e4
Francisco Carriedo Scher <fcarriedos@gmail.com>
2018-04-11 08:47:45 -0400

    Created endpoint for instant online shop

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
 
 var webapp = express();
+
+webapp.use(bodyParser.json());
+webapp.use(bodyParser.urlencoded({ extended: true }));
 
 // Web pages
 webapp
@@ -14,7 +18,11 @@ webapp
   .get('/', (req, res) => res.render('pages/index'));
 
 // Bot endpoints
-webapp.post('/instant-online-shop', (req, res) => res.render('pages/index'));
+webapp.post('/instant-online-shop', (req, res) => {
+	// res.render('pages/index');
+	console.log("Request received: " + JSON.stringify(req.body));
+	res.sendStatus(200);
+});
 
 // And starting!
 webapp.listen(PORT, () => console.log(`Listening on ${ PORT }`));
