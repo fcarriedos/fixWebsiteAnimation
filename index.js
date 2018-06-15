b1aaac550c8b968bdf73d502c85d2ee0dd625ad9
Francisco Carriedo Scher <fcarriedos@gmail.com>
2018-06-15 01:00:52 -0400

    Adding termsAndConditions file.

--- index.js
+++ index.js
@@ -148,5 +148,59 @@ webapp.get('/api/lead/confirm',
 });
 
 
+// Creates the endpoint for our webhook 
+webapp.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  console.log('Request received: ' + JSON.stringify(body, null, 2));
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+    // Returns a '200 OK' response to all requests
+  	res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+// Adds support for GET requests to our webhook
+webapp.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "cacadelavaca"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
+
+// Adds support for GET requests to our webhook
+webapp.get('/privacyAndTerms', (req, res) => {
+    res.status(200).send();
+});
+
+
 // And starting!
 webapp.listen(PORT, () => console.log(`Listening on ${ PORT }`));
