c9c62a4a03f792b4e02aa7de476e903129061bd4
Francisco Carriedo Scher <fcarriedos@gmail.com>
2018-07-13 09:27:39 -0400

    Lead capture through the Chatfuel bot.

--- index.js
+++ index.js
@@ -109,6 +109,60 @@ webapp.post('/api/lead',
 	}
 );
 
+// Send email to waiting list
+webapp.post('/api/lead/bot', 
+	// Validators
+	[
+
+	check('email_by_user').isEmail().withMessage('Is not a valid email.'),
+	check('first_name').exists().custom((value) => value.length > 0).withMessage('The name is empty'),
+	check('last_name').exists().custom((value) => value.length > 0).withMessage('The name is empty')
+
+	],
+	// Request processing
+	(req, res) => {
+
+		console.log(JSON.stringify(req.body, null, 2));
+
+		// errors = validationResult(req);
+		// if (!errors.isEmpty()) {
+		// 	// Treat errors pointing to blocks.
+		// 	return res.status(422).json({ code: 422, messages: errors.mapped() });
+		// } else {
+
+		// 	var email = req.body.email;
+		// 	var name = req.body.name;
+		// 	console.log('Received referer token: ' + req.body.referer_token);
+		// 	// JSON parsing twice because of escaped quotes.
+		// 	var refererDatastructure = JSON.parse(JSON.parse((encryptionUtility.decypher(req.body.referer_token))));
+		// 	var refererId = (refererDatastructure == null) ? null : refererDatastructure.id;
+		// 	console.log('index.post(/api/lead): lead refered by user ' + refererId);
+		// 	// DB checking
+		// 	dbclient.connect(CONSTANTS.MONGODB_URL, function(dbConnectionErr, db) {
+		//         if (dbConnectionErr) { // Crash connecting to the database 
+		//         	console.log('index.post(/api/lead): could not connect to the database to validate ' + email + ' due to ' + dbConnectionErr);
+		//         	throw new Error('index.post(/api/lead): internal server error.');
+		//         } else {
+		// 	        var dbo = db.db(CONSTANTS.WEBSITE_DATABASE_NAME);
+		// 	        dbo.collection(CONSTANTS.SENT_EMAILS_TABLE).findOne({ email: email }, function(err, result) {
+		// 	        	if (err) { // Crash finding the email in the database
+		// 	        		console.log('index.post(/api/lead): error finding email ' + email);
+		// 	        		throw err;
+		// 	        	} else {				       
+		// 		        	if (result != null) {
+		// 		        		return res.status(409).json({ code: 409, messages: ['email already exists'] });
+		// 		        	} else {
+		// 		        		sendgridMailer.sendWaitingListEmail(name, email, refererId, res);
+		// 		        	}
+		// 		        }
+		// 	        	db.close();
+		// 	        });
+		//     	}
+		//     });
+		// }
+	}
+);
+
 
 webapp.get('/api/lead/confirm',
 	// Validators
